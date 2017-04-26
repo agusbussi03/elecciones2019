3e53450208e2e8d33c1b05f3674a14dc8cadb235
Pablo Bussi <pbussi@gmail.com>
2017-04-26 19:10:03 -0300

    Mesa

--- src/Mesa.php
+++ src/Mesa.php
@@ -6,35 +6,53 @@ namespace Elecciones\Common;
  */
 class Mesa
 {
-    /**
-     * Identificador.
-     * @var string
-     */
-    protected $id = '';
+   
+    private $tipo = '';
+    private $sec = 0;
+    private $cirnro = 0;
+    private $cirlet = '';
+    private $app;
 
-    /**
-     * Crea nueva mesa
-     * @param string|null $id          Numero y tipo de mesa: '' para nacional, 'E' para extranjera
-     */
-    public function __construct($id = null)
-    {
-        $this->id = $id."E";
+    function __construct($tipo, $sec, $cirnro, $cirlet, $app) {
+        $this->app = $app;
+        $this->tipo = $tipo;
+        $this->sec = $sec;
+        $this->cirnro = $cirnro;
+        $this->cirlet = $cirlet;
     }
 
-    public function getId()
-    {
-      $zonas=$app['db']->fetchAll('SELECT * FROM zonas');
-        return serialize($zonas);
+    function getFiltros() {
+        switch ($this->tipo) {
+            case 'G': $sql = "SELECT distinct pspar,parnombre,pslista,nombre FROM `actapartido` WHERE psgob='R'";
+                break;
+            case 'D': $sql = "SELECT distinct pspar,parnombre,pslista,nombre FROM `actapartido` WHERE psdip='R'";
+                break;
+            case 'S': $sql = "SELECT distinct pspar,parnombre,pslista,nombre FROM `actapartido` WHERE pssen='R' and sec=".$this->sec;
+                break;
+            case 'I': $sql = "SELECT distinct pspar,parnombre,pslista,nombre FROM `actapartido` WHERE pssen='R' and sec=$this->sec and cirnro=$this->cirnro and cirlet='$this->cirlet'";
+                break;
+            case 'C': $sql = "SELECT distinct pspar,parnombre,pslista,nombre FROM `actapartido` WHERE pssen='R' and sec=$this->sec and cirnro=$this->cirnro and cirlet='$this->cirlet'";
+                break;
+        }
+        $disponibles = $this->app['db']->fetchAll($sql);
+        $filtros = $this->app['db']->fetchAll("SELECT * FROM `filtros` WHERE tipo='$this->tipo'"
+                . " and sec='$this->sec' and cirnro='$this->cirnro' and cirlet='$this->cirlet'");
+        return array('disponibles' => $disponibles, 'filtros' => $filtros);
     }
 
+    function procesar($datos) {
+        $sql = "DELETE from filtros "
+                . "WHERE sec=? and cirnro=? and cirlet=? and tipo=?";
+        $this->app['db']->executeQuery($sql, array((int) $this->sec, (int) $this->cirnro, $this->cirlet, $this->tipo));
 
-
-
+        foreach ($datos as $dato => $valor) {
+            $dato = explode("-", $dato);
+            //print_r($dato);
+            $sql = "INSERT INTO filtros VALUES (?,?,?,?,?,?);";
+            $this->app['db']->executeQuery($sql, array((int) $this->sec, (int) $this->cirnro, $this->cirlet, (int) $dato[0], (int) $dato[1], $this->tipo));
+        }
+        return "Datos modificados";
+    }
 
 }
 
-
-
-
-
- ?>
