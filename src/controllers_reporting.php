59fc7cdfa2ca3f913e0a9ed3b0dc6cd3c221fe07
Bussi Pablo <pbussi@santafe.gov.ar>
2017-06-05 15:11:07 -0300

    panel concejales

--- src/controllers_reporting.php
+++ src/controllers_reporting.php
@@ -1,67 +1,31 @@
 <?php
 
-/*
-  $app->get('/rep_circuito', function () use ($app) {
-  if (!validar('admin')) {
-  return $app->redirect($app['url_generator']->generate('login'));
-  }
-  $sql = "SELECT m.sec,m.cirnro,m.cirlet,c.nomb,count(*) as cuenta from mesas m,circuitos c "
-  . "where m.sec=c.sec and m.cirnro=c.cirnro and m.cirlet=c.cirlet and m.testigo=1 and m.sec not in (9,13,14) "
-  . "group by m.sec,m.cirnro,m.cirlet,c.nomb "
-  . "UNION select sec,9999,'','SANTA FE',count(*) as cuenta from mesas where sec=9 and testigo=1 group by sec "
-  . "UNION select 13,9999,'','ROSARIO',count(*) as cuenta from mesas where sec in (13,14) and testigo=1 group by sec";
-  $resultado = $app['db']->fetchAll($sql);
-  foreach ($resultado as $item) {
-  $sec = $item['sec'];
-  $cirnro = $item['cirnro'];
-  $cirlet = $item['cirlet'];
-  if ($sec == 9) {
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? "
-  . "and mesa not in (select mesa from renglon where con>0)";
-  $mesas_nocargadas_C = $app['db']->fetchAll($sql, array((int) $sec));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? "
-  . "and mesa  in (select mesa from renglon where con>0)";
-  $mesas_cargadas_C = $app['db']->fetchAll($sql, array((int) $sec));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? "
-  . "and mesa not in (select mesa from renglon where inte>0)";
-  $mesas_nocargadas_I = $app['db']->fetchAll($sql, array((int) $sec));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? "
-  . "and mesa  in (select mesa from renglon where inte>0)";
-  $mesas_cargadas_I = $app['db']->fetchAll($sql, array((int) $sec));
-  } elseif ($sec == 13 || $sec == 14) {
-  $sql = "SELECT * FROM mesas where testigo=1 and sec in (13,14)"
-  . "and mesa not in (select mesa from renglon where con>0)";
-  $mesas_nocargadas_C = $app['db']->fetchAll($sql);
-  $sql = "SELECT * FROM mesas where testigo=1 and sec in (13,14)"
-  . "and mesa  in (select mesa from renglon where con>0)";
-  $mesas_cargadas_I = $app['db']->fetchAll($sql);
-  $sql = "SELECT * FROM mesas where testigo=1 and sec in (13,14)"
-  . "and mesa not in (select mesa from renglon where inte>0)";
-  $mesas_nocargadas_C = $app['db']->fetchAll($sql);
-  $sql = "SELECT * FROM mesas where testigo=1 and sec in (13,14)"
-  . "and mesa  in (select mesa from renglon where inte>0)";
-  $mesas_cargadas_I = $app['db']->fetchAll($sql);
-  } else {
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? and cirnro=? and cirlet=? "
-  . "and mesa not in (select mesa from renglon where con>0)";
-  $mesas_nocargadas_C = $app['db']->fetchAll($sql, array((int) $sec, (int) $cirnro, $cirlet));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? and cirnro=? and cirlet=? "
-  . "and mesa in (select mesa from renglon where con>0)";
-  $mesas_cargadas_C = $app['db']->fetchAll($sql, array((int) $sec, (int) $cirnro, $cirlet));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? and cirnro=? and cirlet=? "
-  . "and mesa not in (select mesa from renglon where inte>0)";
-  $mesas_nocargadas_I = $app['db']->fetchAll($sql, array((int) $sec, (int) $cirnro, $cirlet));
-  $sql = "SELECT * FROM mesas where testigo=1 and sec=? and cirnro=? and cirlet=? "
-  . "and mesa in (select mesa from renglon where inte>0)";
-  $mesas_cargadas_I = $app['db']->fetchAll($sql, array((int) $sec, (int) $cirnro, $cirlet));
-  }
-  $circuitos[] = array('datos' => $item,
-  'concejal' => array('cargadas' => $mesas_cargadas_C, 'nocargadas' => $mesas_nocargadas_C),
-  'intendente' => array('cargadas' => $mesas_cargadas_I, 'nocargadas' => $mesas_nocargadas_I,));
-  }
-  //print_r($circuitos);
-  return $app['twig']->render('rep_circuitos.html.twig', array('circuitos' => $circuitos));
-  })->bind('rep_circuitos'); */
+$app->get('/rep_circuito', function () use ($app) {
+    if (!validar('admin')) {
+        return $app->redirect($app['url_generator']->generate('login'));
+    }
+    $sql = "SELECT distinct c.* from circuito c, mesa m where c.id=m.circuito_id";
+    $resultado = $app['db']->fetchAll($sql);
+    foreach ($resultado as $item) {
+        $sql = "SELECT * FROM mesa where circuito_id=" . $item['id'] . " and "
+                . "id not in (select mesa_id from renglon where concejal>0)";
+        $mesas_nocargadas_C = $app['db']->fetchAll($sql);
+        $sql = "SELECT * FROM mesa where circuito_id=" . $item['id'] . " and "
+                . "id  in (select mesa_id from renglon where concejal>0)";
+        $mesas_cargadas_C = $app['db']->fetchAll($sql);
+        $sql = "SELECT * FROM mesa where circuito_id=" . $item['id'] . " and "
+                . "id not in (select mesa_id from renglon where intendente>0)";
+        $mesas_nocargadas_I = $app['db']->fetchAll($sql);
+        $sql = "SELECT * FROM mesa where circuito_id=" . $item['id'] . " and "
+                . "id  in (select mesa_id from renglon where intendente>0)";
+        $mesas_cargadas_I = $app['db']->fetchAll($sql);
+
+        $circuitos[] = array('datos' => $item,
+            'concejal' => array('cargadas' => $mesas_cargadas_C, 'nocargadas' => $mesas_nocargadas_C),
+            'intendente' => array('cargadas' => $mesas_cargadas_I, 'nocargadas' => $mesas_nocargadas_I,));
+    }
+    return $app['twig']->render('rep_circuitos.html.twig', array('circuitos' => $circuitos));
+})->bind('rep_circuito');
 
 
 $app->get('/rep_concejales_seccional/{tipo}/{id}', function ($tipo, $id) use ($app) {
@@ -80,7 +44,7 @@ $app->get('/rep_concejales_seccional/{tipo}/{id}', function ($tipo, $id) use ($a
     if ($tipo == 'porcentajes') {
         $resultado = $concejales->getPorcentajes();
         //echo "SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
-       // print_r($resultado);
+        // print_r($resultado);
         return $app['twig']->render('reporting/res_concejales_porcentaje.html.twig', array('votos' => $resultado['porcentajes'], 'circuito' => $circuito,
                     'totales' => $resultado['totales_porcentajes'], 'seccionales' => $concejales->getSeccionales()));
     }
@@ -92,8 +56,8 @@ $app->get('/rep_concejales_seccional/{tipo}/{id}', function ($tipo, $id) use ($a
     }
     if ($tipo == 'graficos') {
         $resultado = $concejales->getPorcentajeponderado();
-        $partidos=$concejales->getPartidos();
-        return $app['twig']->render('reporting/res_concejales_grafico.html.twig', array('totales' => $resultado, 'circuito' => $circuito,'partidos'=>$partidos));
+        $partidos = $concejales->getPartidos();
+        return $app['twig']->render('reporting/res_concejales_grafico.html.twig', array('totales' => $resultado, 'circuito' => $circuito, 'partidos' => $partidos));
     }
     if ($tipo == 'distribucion') {
         $resultado = $concejales->getDistribucion();
